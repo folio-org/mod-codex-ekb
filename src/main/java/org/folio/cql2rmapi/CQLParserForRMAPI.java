ca1a9aca2023e499575cc848d66084961f6c2f34
U-CORP\sduvvuri <sduvvuri@LP10-5CG6445J3Q.corp.epnet.com>
2017-12-06 10:24:26 -0500

    CQL Parser Unit Tests

--- src/main/java/org/folio/cql2rmapi/CQLParserForRMAPI.java
+++ src/main/java/org/folio/cql2rmapi/CQLParserForRMAPI.java
@@ -15,16 +15,22 @@ import org.z3950.zing.cql.CQLSortNode;
 import org.z3950.zing.cql.CQLTermNode;
 import org.z3950.zing.cql.ModifierSet;
 
+/**
+ * This class is responsible for parsing CQL query to an extent that we extract
+ * the supported features of RM API, translate, construct a query string and
+ * return it to the CodexInstancesResourceImpl class.
+ */
+
 public class CQLParserForRMAPI {
 
 	private String error = "Unsupported Query Format : ";
 	private static final String RM_API_TITLE = "titlename";
 	private static final String TITLE = "title";
-	private String searchField = null;
-	private String searchValue = null;
-	private String filterType = null;
-	private String filterValue = null;
-	private String sortOrder = null;
+	String searchField = null;
+	String searchValue = null;
+	String filterType = null;
+	String filterValue = null;
+	String sortOrder = null;
 	private int offset;
 	private final int limit;
 
@@ -33,102 +39,107 @@ public class CQLParserForRMAPI {
 	}
 
 	private enum RMAPISupportedFilterValues {
-		ALL, JOURNAL, NEWSLETTER, REPORT, PROCEEDINGS, WEBSITE, NEWSPAPER, UNSPECIFIED, BOOK, BOOKSERIES,
-		DATABASE, THESISDISSERTATION, STREAMINGAUDIO, STREAMINGVIDEO, AUDIOBOOK
+		ALL, JOURNAL, NEWSLETTER, REPORT, PROCEEDINGS, WEBSITE, NEWSPAPER, UNSPECIFIED, BOOK, BOOKSERIES, DATABASE, THESISDISSERTATION, STREAMINGAUDIO, STREAMINGVIDEO, AUDIOBOOK
 	}
 
 	public CQLParserForRMAPI(String query, int offset, int limit) throws QueryValidationException {
 		this.offset = offset;
 		this.limit = limit;
-	    initCQLParser(query);
+		final CQLNode node = initCQLParser(query);
+		checkNodeInstance(node);
 	}
 
-	private void initCQLParser(String query) throws QueryValidationException {
-		  final CQLParser parser = new CQLParser();
-			try {
-				final CQLNode node = parser.parse(query);
-				checkNodeInstance(node);
-			} catch (CQLParseException | IOException e) {
-				error += "Search query is in an unsupported format.";
-				throw new QueryValidationException(error);
-			}
-	  }
+	CQLNode initCQLParser(String query) throws QueryValidationException {
+		final CQLParser parser = new CQLParser();
+		try {
+			return parser.parse(query);
+		} catch (CQLParseException | IOException e) {
+			error += "Search query is in an unsupported format.";
+			throw new QueryValidationException(error);
+		}
+	}
 
-
-	  private void checkNodeInstance(CQLNode node) throws QueryValidationException{
-		  if(node instanceof CQLTermNode) {
-			  parseCQLTermNode((CQLTermNode) node);
-		  }
-		  if(node instanceof CQLBooleanNode) {
-			  parseCQLBooleanNode((CQLBooleanNode) node);
-		  }
-		  if(node instanceof CQLSortNode) {
-			  parseCQLSortNode((CQLSortNode) node);
-		  }
-	  }
+	private void checkNodeInstance(CQLNode node) throws QueryValidationException {
+		if (node instanceof CQLTermNode) {
+			parseCQLTermNode((CQLTermNode) node);
+		}
+		if (node instanceof CQLBooleanNode) {
+			parseCQLBooleanNode((CQLBooleanNode) node);
+		}
+		if (node instanceof CQLSortNode) {
+			parseCQLSortNode((CQLSortNode) node);
+		}
+	}
 
 	private void parseCQLTermNode(CQLTermNode node) throws QueryValidationException {
-		  final String comparator = node.getRelation().getBase(); //gives operator
+		final String comparator = node.getRelation().getBase(); // gives operator
 
-		  //If comparison operators are not supported, log and return an error response
-		  if(!comparator.equals("=")) {
-			  error += "Search with " + comparator + " operator is not supported.";
-			  throw new QueryValidationException(error);
-		  }
-
-		  final String indexNode = node.getIndex(); //gives the search field
-		  final String termNode = node.getTerm(); //gives the search value
-
-		  //If no search field is passed, default it to title search. This is the default search supported by RMAPI
-		  if ("cql.serverChoice".equalsIgnoreCase(indexNode)) {
-			  searchField = RM_API_TITLE;
-			  searchValue = termNode;
-		  } else if ("identifier.type".equalsIgnoreCase(indexNode) && EnumUtils.isValidEnum(RMAPISupportedSearchFields.class, termNode.toUpperCase())) {
-			  searchField = termNode;
-		  } else if ("identifier.value".equalsIgnoreCase(indexNode)) {
-			  searchValue = termNode;
-		  } else if ("type".equalsIgnoreCase(indexNode)) {
-			  filterType = indexNode;
-			  filterValue = termNode;
-		  } else if(!EnumUtils.isValidEnum(RMAPISupportedSearchFields.class, indexNode.toUpperCase())) {
-			  //If search field is not supported, log and return an error response
-			  error += "Search field " + indexNode + " is not supported.";
-			  throw new QueryValidationException(error);
-		  } else {
-			  searchField = indexNode;
-			  searchValue = termNode;
-		  }
+		// If comparison operators are not supported, log and return an error response
+		if (!comparator.equals("=")) {
+			error += "Search with " + comparator + " operator is not supported.";
+			throw new QueryValidationException(error);
 		}
 
-	  private void parseCQLSortNode(CQLSortNode node) throws QueryValidationException {
-		  final List<ModifierSet> sortIndexes = node.getSortIndexes();
-		  if(sortIndexes.size() > 1) {
-			  error += "Sorting on multiple keys is unsupported.";
-			  throw new QueryValidationException(error);
-		  }
-		  //At this point RM API supports only sort by title and relevance
-		  //front end does not support relevance, so we ignore everything but title
-		  for(final ModifierSet ms : sortIndexes) {
-			  sortOrder = ms.getBase();
-			 if(sortOrder.equalsIgnoreCase(TITLE)) {
-				 sortOrder = RM_API_TITLE;
-			 }else {
-				 final StringBuilder builder = new StringBuilder();
-				 builder.append(error);
-				 builder.append("Sorting on ");
-				 builder.append(sortOrder);
-				 builder.append(" key is unsupported.");
-				 throw new QueryValidationException(builder.toString());
-			 }
-		  }
+		final String indexNode = node.getIndex(); // gives the search field
+		final String termNode = node.getTerm(); // gives the search value
 
-		  //Get the search field and search value
-		  final CQLNode subTree = node.getSubtree();
-		  checkNodeInstance(subTree);
+		// If no search field is passed, default it to title search. This is the default
+		// search supported by RMAPI
+		if ("cql.serverChoice".equalsIgnoreCase(indexNode)) {
+			searchField = RM_API_TITLE;
+			searchValue = termNode;
+		} else if ("identifier.type".equalsIgnoreCase(indexNode)
+				&& EnumUtils.isValidEnum(RMAPISupportedSearchFields.class, termNode.toUpperCase())) {
+			searchField = termNode;
+		} else if ("identifier.value".equalsIgnoreCase(indexNode)) {
+			searchValue = termNode;
+		} else if ("type".equalsIgnoreCase(indexNode)) {
+			filterType = indexNode;
+			filterValue = termNode;
+			if((filterValue != null) && !EnumUtils.isValidEnum(RMAPISupportedFilterValues.class, filterValue.toUpperCase())) {
+				// If filter value is not supported, log and return an error response
+				error += "Filter on resource type whose value is " + filterValue + " is not supported.";
+				throw new QueryValidationException(error);
+			}
+		} else if (!EnumUtils.isValidEnum(RMAPISupportedSearchFields.class, indexNode.toUpperCase())) {
+			// If search field is not supported, log and return an error response
+			error += "Search field " + indexNode + " is not supported.";
+			throw new QueryValidationException(error);
+		} else {
+			searchField = indexNode;
+			searchValue = termNode;
+		}
+	}
+
+	private void parseCQLSortNode(CQLSortNode node) throws QueryValidationException {
+		final List<ModifierSet> sortIndexes = node.getSortIndexes();
+		if (sortIndexes.size() > 1) {
+			error += "Sorting on multiple keys is unsupported.";
+			throw new QueryValidationException(error);
+		}
+		// At this point RM API supports only sort by title and relevance
+		// front end does not support relevance, so we ignore everything but title
+		for (final ModifierSet ms : sortIndexes) {
+			sortOrder = ms.getBase();
+			if (sortOrder.equalsIgnoreCase(TITLE)) {
+				sortOrder = RM_API_TITLE;
+			} else {
+				final StringBuilder builder = new StringBuilder();
+				builder.append(error);
+				builder.append("Sorting on ");
+				builder.append(sortOrder);
+				builder.append(" key is unsupported.");
+				throw new QueryValidationException(builder.toString());
+			}
+		}
+
+		// Get the search field and search value
+		final CQLNode subTree = node.getSubtree();
+		checkNodeInstance(subTree);
 	}
 
 	private void parseCQLBooleanNode(CQLBooleanNode node) throws QueryValidationException {
-		if(node instanceof CQLAndNode) {
+		if (node instanceof CQLAndNode) {
 			final CQLNode leftNode = node.getLeftOperand();
 			checkNodeInstance(leftNode);
 			final CQLNode rightNode = node.getRightOperand();
@@ -138,10 +149,10 @@ public class CQLParserForRMAPI {
 		}
 	}
 
-	public String getRMAPIQuery() throws UnsupportedEncodingException {
+	public String getRMAPIQuery() throws QueryValidationException {
 		final StringBuilder builder = new StringBuilder();
 
-		if((searchValue != null) && (searchField != null)) {
+		if ((searchValue != null) && (searchField != null)) {
 			// Map fields to RM API
 
 			if (searchField.equalsIgnoreCase(TITLE)) {
@@ -150,26 +161,31 @@ public class CQLParserForRMAPI {
 			if (searchField.equals("isbn") || searchField.equals("issn")) {
 				searchField = "isxn";
 			}
-			if(sortOrder == null) {
-				sortOrder = RM_API_TITLE; //orderby is a mandatory field, otherwise RMAPI throws error
+			if (sortOrder == null) {
+				sortOrder = RM_API_TITLE; // orderby is a mandatory field, otherwise RMAPI throws error
 			}
-			if(offset == 0) {
-				offset = 1; //calculate offsets correctly
+			if (offset == 0) {
+				offset = 1; // calculate offsets correctly
 			}
 			builder.append("search=");
-			builder.append(URLEncoder.encode(searchValue, "UTF-8"));
+			try {
+				builder.append(URLEncoder.encode(searchValue, "UTF-8"));
+			} catch (final UnsupportedEncodingException e) {
+				throw new QueryValidationException(e);
+			}
 			builder.append("&searchfield=" + searchField);
+
+			if ((filterType != null) && (filterValue != null)) {
+				// Map fields to RM API
+				builder.append("&resourcetype=" + filterValue);
+			}
+			builder.append("&orderby=" + sortOrder);
+
+			builder.append("&count=" + limit);
+			builder.append("&offset=" + offset);
+		}else {
+			throw new QueryValidationException("Invalid query format, issue with search parameters");
 		}
-
-		if(((filterType != null) && filterType.equalsIgnoreCase("type")) && ((filterValue != null) && EnumUtils.isValidEnum(RMAPISupportedFilterValues.class, filterValue.toUpperCase()))) {
-			//Map fields to RM API
-			builder.append("&resourcetype=" + filterValue);
-		}
-		builder.append("&orderby=" + sortOrder);
-
-		builder.append("&count=" + limit);
-		builder.append("&offset=" + offset);
-
 		return builder.toString();
 	}
 }
---
+++ src/test/java/org/folio/cql2rmapi/CQLParserForRMAPITest.java
@@ -0,0 +1,200 @@
+package org.folio.cql2rmapi;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.easymock.EasyMock;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.z3950.zing.cql.CQLNode;
+
+/**
+ * Unit tests for CQLParserForRMAPI class.
+ *
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(CQLParserForRMAPI.class)
+public class CQLParserForRMAPITest {
+
+	private static final String VALID_QUERY = "title=bridget";
+	private static final String VALID_ISBN_QUERY = "identifier.type = isbn and identifier.value = 12345 sortby title";
+	private static final String VALID_ISSN_QUERY = "identifier.type = issn and identifier.value = 12345 sortby title";
+	private static final String VALID_FILTER_QUERY = "title = bridget and type = journal sortby title";
+
+	@Test(expected = QueryValidationException.class)
+	public void initCQLParserThrowsExceptionIfQueryInvalidTest() throws QueryValidationException {
+		final String invalidQuery = "";
+		new CQLParserForRMAPI(invalidQuery, 1, 10);
+	}
+
+	@Test
+	public void initCQLParserReturnsInstanceOfCQLNodeIfQueryValidTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_QUERY, 1, 10);
+		final Object returnValue = parser.initCQLParser(VALID_QUERY);
+		assertThat(returnValue, instanceOf(CQLNode.class));
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionInCaseOfUnsupportedOperatorTest() throws QueryValidationException {
+		final String invalidOperator = "title<>bridget";
+		new CQLParserForRMAPI(invalidOperator, 1, 10);
+	}
+
+	@Test
+	public void CQLParserSetsExpectedValuesIfSearchFieldNotProvidedTest() throws QueryValidationException {
+		final String validQuery = "bridget";
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(validQuery, 1, 10);
+		assertEquals("titlename", parser.searchField);
+		assertEquals("bridget", parser.searchValue);
+	}
+
+	@Test
+	public void CQLParserSetsExpectedValuesIfValidSearchFieldIsProvidedTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_ISBN_QUERY, 1, 10);
+		assertEquals("isbn", parser.searchField);
+		assertEquals("12345", parser.searchValue);
+		assertEquals("titlename", parser.sortOrder);
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionInCaseOfUnsupportedIdentifierFieldTest() throws QueryValidationException {
+		final String invalidIdentifierTypeQuery = "identifier.type = xyz and identifier.value = 12345 sortby title";
+		new CQLParserForRMAPI(invalidIdentifierTypeQuery, 1, 10);
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionInCaseOfUnsupportedSearchFieldTest() throws QueryValidationException {
+		final String invalidOperator = "author=bridget";
+		new CQLParserForRMAPI(invalidOperator, 1, 10);
+	}
+
+	@Test
+	public void CQLParserSetsExpectedValuesIfTypeFilterIsPassedTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_FILTER_QUERY, 1, 10);
+		assertEquals("title", parser.searchField);
+		assertEquals("bridget", parser.searchValue);
+		assertEquals("titlename", parser.sortOrder);
+		assertEquals("type", parser.filterType);
+		assertEquals("journal", parser.filterValue);
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfInvalidFilterValueIsPassedTest() throws QueryValidationException {
+		final String invalidFilterQuery = "title = bridget and type = hardcopy sortby title";
+		new CQLParserForRMAPI(invalidFilterQuery, 1, 10);
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfNullFilterValueIsPassedTest() throws QueryValidationException {
+		final String invalidFilterQuery = "title = bridget and type = null sortby title";
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(invalidFilterQuery, 1, 10);
+		parser.filterValue = null;
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfMultipleSortKeysAreProvidedTest() throws QueryValidationException {
+		final String multipleSortQuery = "title = bridget and type = journal sortby title, publisher";
+		new CQLParserForRMAPI(multipleSortQuery, 1, 10);
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfSortOnUnsupportedFieldTest() throws QueryValidationException {
+		final String multipleSortQuery = "title = bridget and type = journal sortby publisher";
+		new CQLParserForRMAPI(multipleSortQuery, 1, 10);
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfUnsupportedBooleanOperatorIsPassedTest() throws QueryValidationException {
+		final String multipleSortQuery = "title = bridget OR publisher = Ebsco sortby title";
+		new CQLParserForRMAPI(multipleSortQuery, 1, 10);
+	}
+
+	@Test
+	public void CQLParserReturnsExpectedQueryStringIfValidQueryIsPassedTest() throws QueryValidationException, UnsupportedEncodingException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_QUERY, 0, 0);
+		assertEquals("search=bridget&searchfield=titlename&orderby=titlename&count=0&offset=1", parser.getRMAPIQuery());
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfsearchValueAndSearchFieldAreNullTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_QUERY, 0, 0);
+		parser.searchField = null;
+		parser.searchValue = null;
+		parser.getRMAPIQuery();
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfsearchValueIsNullTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_QUERY, 0, 0);
+		parser.searchValue = null;
+		parser.getRMAPIQuery();
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionIfsearchFieldIsNullTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_QUERY, 0, 0);
+		parser.searchField = null;
+		parser.getRMAPIQuery();
+	}
+
+	@Test
+	public void CQLParserMapsISBNToISXNTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_ISBN_QUERY, 0, 0);
+		assertEquals("search=12345&searchfield=isxn&orderby=titlename&count=0&offset=1", parser.getRMAPIQuery());
+	}
+
+	@Test
+	public void CQLParserMapsISSNToISXNTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_ISSN_QUERY, 0, 0);
+		assertEquals("search=12345&searchfield=isxn&orderby=titlename&count=0&offset=1", parser.getRMAPIQuery());
+	}
+
+	@Test
+	public void CQLParserPassesAlongOffsetIfNotEqualToZeroTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_ISSN_QUERY, 10, 0);
+		assertEquals("search=12345&searchfield=isxn&orderby=titlename&count=0&offset=10", parser.getRMAPIQuery());
+	}
+
+	@Test
+	public void CQLParserDoesNotAppendResourceTypeIfTypeNotPassedAlongTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_ISSN_QUERY, 10, 0);
+		assertEquals("search=12345&searchfield=isxn&orderby=titlename&count=0&offset=10", parser.getRMAPIQuery());
+	}
+
+	@Test
+	public void CQLParserDoesNotAppendResourceTypeIfFilterTypeIsNullTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_FILTER_QUERY, 10, 0);
+		parser.filterType = null;
+		assertEquals("search=bridget&searchfield=titlename&orderby=titlename&count=0&offset=10", parser.getRMAPIQuery());
+	}
+
+	@Test
+	public void CQLParserDoesNotAppendResourceTypeIfFilTerValueIsNullTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_FILTER_QUERY, 10, 0);
+		parser.filterValue = null;
+		assertEquals("search=bridget&searchfield=titlename&orderby=titlename&count=0&offset=10", parser.getRMAPIQuery());
+	}
+
+	@Test
+	public void CQLParserAppendsResourceTypeIfFilterTypeAndFilTerValueAreValidTest() throws QueryValidationException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_FILTER_QUERY, 10, 0);
+		assertEquals("search=bridget&searchfield=titlename&resourcetype=journal&orderby=titlename&count=0&offset=10", parser.getRMAPIQuery());
+	}
+
+	@Test(expected = QueryValidationException.class)
+	public void CQLParserThrowsExceptionWhenURLEncodingFails() throws QueryValidationException, UnsupportedEncodingException {
+		final CQLParserForRMAPI parser = new CQLParserForRMAPI(VALID_FILTER_QUERY, 10, 0);
+		mockStatic(URLEncoder.class);
+		EasyMock.expect(URLEncoder.encode("bridget", "UTF-8")).andThrow(new UnsupportedEncodingException());
+		replayAll();
+		parser.getRMAPIQuery();
+	}
+}
