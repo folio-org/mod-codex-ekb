914b528c3844b0797a05a59e9d5c44ba5070236f
mreno-EBSCO@users.noreply.github.com <mreno-EBSCO@users.noreply.github.com>
2018-01-04 14:31:34 -0500

    Implement MODCXEKB-52 - Search by ID
    Added support to search by ID when the CQL parser indicates that the
    search field is "id". In this case, since there is no mechanism to
    search by KBID in the RM API and it is not possible or practical to
    retrieve all titles and check the KBID sequentially, we do not make the
    RM API call to search titles, rather we make the RM API call to retrieve
    a title by ID.
    
    If the title is found, we convert to an instance and store the instance
    in an InstanceCollection of length 1. If the title is not found, we
    return an InstanceCollection of length 0.
    
    Also fixed a problem with the PACT test affecting the FOLIO
    HttpClientFactory mocking setting depending on the order the tests are
    executed. We now assume mocking is enabled whenever the tests are run
    (the PACT test needs to disable mocking to execute properly, however
    when complete it will now always turn it back on, even if it was not on
    prior to the test, i.e. the PACT test was the first test executed).

--- src/test/java/org/folio/utils/Utils.java
+++ src/test/java/org/folio/utils/Utils.java
@@ -4,14 +4,22 @@
 package org.folio.utils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.util.Random;
+
+import org.apache.commons.io.IOUtils;
+
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 
 /**
  * @author mreno
  *
  */
 public final class Utils {
+  private static final Logger logger = LoggerFactory.getLogger("okapi");
+
   private Utils() {
     super();
   }
@@ -25,10 +33,10 @@ public final class Utils {
   public static int getRandomPort() {
     int port = -1;
     do {
-      // Use a random ephemeral port if not defined via a system property
+      // Use a random ephemeral port
       port = new Random().nextInt(16_384) + 49_152;
       try {
-        ServerSocket socket = new ServerSocket(port);
+        final ServerSocket socket = new ServerSocket(port);
         socket.close();
       } catch (IOException e) {
         continue;
@@ -37,5 +45,21 @@ public final class Utils {
     } while (true);
 
     return port;
+  }
+
+  public static String readMockFile(final String path) {
+    try {
+      final InputStream is = Utils.class.getClassLoader().getResourceAsStream(path);
+
+      if (is != null) {
+        return IOUtils.toString(is, "UTF-8");
+      } else {
+        return "";
+      }
+    } catch (Throwable e) {
+      logger.error(String.format("Unable to read mock configuration in %s file", path));
+    }
+
+    return "";
   }
 }
