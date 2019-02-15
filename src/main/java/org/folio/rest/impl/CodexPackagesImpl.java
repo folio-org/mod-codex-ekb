544b110d7260c3e8ea8a5ad3b7fcb4371aa1ce35
carolegodfrey <cgodfrey@ebsco.com>
2019-02-15 16:11:12 -0500

    Add placeholders for package methods

---
+++ src/main/java/org/folio/rest/impl/CodexPackagesImpl.java
@@ -0,0 +1,29 @@
+package org.folio.rest.impl;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Context;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import org.folio.rest.jaxrs.resource.CodexPackages;
+import javax.ws.rs.core.Response;
+import java.util.Map;
+
+/**
+ * Package related codex APIs.
+ */
+public final class CodexPackagesImpl implements CodexPackages {
+  private final Logger log = LoggerFactory.getLogger(CodexPackagesImpl.class);
+
+
+  @Override
+  public void getCodexPackages(String query, int offset, int limit, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    asyncResultHandler.handle(Future.succeededFuture(Response.status(Response.Status.NOT_IMPLEMENTED).build()));
+  }
+
+  @Override
+  public void getCodexPackagesById(String id, String lang, Map<String, String> okapiHeaders, Handler<AsyncResult<Response>> asyncResultHandler, Context vertxContext) {
+    asyncResultHandler.handle(Future.succeededFuture(Response.status(Response.Status.NOT_IMPLEMENTED).build()));
+  }
+}
