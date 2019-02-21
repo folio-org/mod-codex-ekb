b73508730c2a4594da4dee2b7e969c6eb64a9816
Andrii Paias <Andrii_Paias@epam.com>
2019-02-21 12:49:35 +0200

    Implement /codex-packages-sources
    Jira: MODCXEKB-81
    Use PomReader.INSTANCE to get module name and version

--- src/test/java/org/folio/rest/impl/CodexInstanceResourceImplTest.java
+++ src/test/java/org/folio/rest/impl/CodexInstanceResourceImplTest.java
@@ -4,35 +4,23 @@ import static org.folio.utils.Utils.readMockFile;
 import static org.hamcrest.Matchers.containsString;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.folio.rest.RestVerticle;
-import org.folio.rest.tools.PomReader;
-import org.folio.rest.tools.client.test.HttpClientMock2;
-import org.folio.utils.Utils;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.http.Header;
 import io.restassured.response.Response;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 
 @RunWith(VertxUnitRunner.class)
-public class CodexInstanceResourceImplTest {
-  private static final String MOCK_RMAPI_CONFIG_SUCCESS_FILE = "RMAPIToCodex/mock_content.json";
+public class CodexInstanceResourceImplTest extends VertxTestBase {
+
   private static final String MOCK_RMAPI_INSTANCE_TITLE_200_RESPONSE_WHEN_FOUND = "RMAPIService/SuccessGetTitleById.json";
   private static final String MOCK_RMAPI_INSTANCE_TITLE_404_RESPONSE_WHEN_NOT_FOUND = "RMAPIConfiguration/mock_content_fail_404.json";
   private static final String MOCK_CODEX_INSTANCE_TITLE_COLLECTION_200_RESPONSE_WHEN_FOUND = "RMAPIService/SuccessGetTitleList.json";
@@ -41,56 +29,24 @@ public class CodexInstanceResourceImplTest {
   private static final String SEARCH_TITLE_COLLECTION_WHEN_SEARCH_FIELD_NOT_GIVEN_SUCCESS_QUERY = "Bridget Jones";
   private static final String SEARCH_TITLE_COLLECTION_FAILS_UNSUPPORTED_QUERY = "title = Bridget Jones or publisher = xyz";
 
-  private final Logger logger = LoggerFactory.getLogger("CodexInstancesResourceImplTest");
-
-  private final int okapiPort = Utils.getRandomPort();
-
-  private final Header tenantHeader = new Header("X-Okapi-Tenant", "codexinstancesresourceimpltest");
-  private final Header urlHeader = new Header("X-Okapi-Url", "https://localhost:" + okapiPort);
-  private final Header contentTypeHeader = new Header("Content-Type", "application/json");
-
-  private String moduleName;
-  private String moduleVersion;
-  private String moduleId;
-  private Vertx vertx;
-  // This object is needed to modify RMAPIConfiguration's local
-  // object as a side effect.
-  private HttpClientMock2 httpClientMock;
-
   @Before
   public void setUp(TestContext context) {
-    vertx = Vertx.vertx();
-
-    moduleName = PomReader.INSTANCE.getModuleName().replaceAll("_", "-");
-    moduleVersion = PomReader.INSTANCE.getVersion();
-    moduleId = moduleName + "-" + moduleVersion;
-    logger.info("Test setup starting for " + moduleId);
-
-    final JsonObject conf = new JsonObject();
-    conf.put("http.port", okapiPort);
-    conf.put(HttpClientMock2.MOCK_MODE, "true");
-
-    final DeploymentOptions opt = new DeploymentOptions().setConfig(conf);
-    vertx.deployVerticle(RestVerticle.class.getName(), opt, context.asyncAssertSuccess());
-    RestAssured.port = okapiPort;
-    logger.info("Codex Instances Resource Test Setup Done using port " + okapiPort);
-
+    super.setUp(context);
+    final Async async = context.async();
     final int serverPort = Integer.parseInt(System.getProperty("serverPort", Integer.toString(51234)));
     final String host = "localhost";
-
-    final Async async = context.async();
     final HttpServer server = vertx.createHttpServer();
     server.requestHandler(req -> {
       if (req.path().equals(String.format("/rm/rmaccounts/test/titles/%s", "99999"))) {
         req.response().setStatusCode(200).putHeader("content-type", "application/json")
-            .end(readMockFile(MOCK_RMAPI_INSTANCE_TITLE_200_RESPONSE_WHEN_FOUND));
+          .end(readMockFile(MOCK_RMAPI_INSTANCE_TITLE_200_RESPONSE_WHEN_FOUND));
       } else if (req.path().equals(String.format("/rm/rmaccounts/test/titles/%s", "1"))) {
         req.response().setStatusCode(404).putHeader("content-type", "text/plain")
-            .end(readMockFile(MOCK_RMAPI_INSTANCE_TITLE_404_RESPONSE_WHEN_NOT_FOUND));
+          .end(readMockFile(MOCK_RMAPI_INSTANCE_TITLE_404_RESPONSE_WHEN_NOT_FOUND));
       } else if (req.path().equals("/rm/rmaccounts/test/titles")) {
         if (req.uri().contains("searchfield=titlename&search=Bridget+Jones&orderby=titlename&count=10&offset=1")) {
           req.response().setStatusCode(200).putHeader("content-type", "application/json")
-          .end(readMockFile(MOCK_CODEX_INSTANCE_TITLE_COLLECTION_200_RESPONSE_WHEN_FOUND));
+            .end(readMockFile(MOCK_CODEX_INSTANCE_TITLE_COLLECTION_200_RESPONSE_WHEN_FOUND));
         } else {
           req.response().setStatusCode(500).end("Unexpected call: " + req.path());
         }
@@ -98,32 +54,13 @@ public class CodexInstanceResourceImplTest {
         req.response().setStatusCode(500).end("Unexpected call: " + req.path());
       }
     });
-
     server.listen(serverPort, host, ar -> {
       async.complete();
     });
-
-    final Map<String, String> okapiHeaders = new HashMap<>();
-    okapiHeaders.put("X-Okapi-Tenant", "codexinstancesresourceimpltest");
-    okapiHeaders.put("X-Okapi-Url", "https://localhost:" + Integer.toString(okapiPort));
-    httpClientMock = new HttpClientMock2(okapiHeaders.get("X-Okapi-Tenant"), okapiHeaders.get("X-Okapi-Url"));
-    try {
-      // Mocking the RM API Configuration response
-      httpClientMock.setMockJsonContent(MOCK_RMAPI_CONFIG_SUCCESS_FILE);
-    } catch (final IOException e) {
-      context.fail("Cannot read mock file: " + MOCK_RMAPI_CONFIG_SUCCESS_FILE + " - reason: " + e.getMessage());
-    }
-  }
-
-  @After
-  public void tearDown(TestContext context) {
-    logger.info("Codex Instances Resource Testing Complete");
-    vertx.close(context.asyncAssertSuccess());
   }
 
   @Test
   public void getCodexInstancesByIdSuccessTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Testing for successful instance id");
 
     final Response r = RestAssured
@@ -145,12 +82,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesByIdThrowsExceptionWhenOkapiURLIsEmptyTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Test when Okapi URL is null is starting");
 
     RestAssured
@@ -165,12 +100,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesByIdTitleNotFoundTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Testing for response when title not found");
 
     RestAssured
@@ -186,12 +119,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesByIdTitleNotAuth(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Testing for response when not authorized");
 
     try {
@@ -214,12 +145,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesSuccessTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Testing for successful instance collection");
 
     final Response r = RestAssured
@@ -244,12 +173,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesIdSearchSuccessTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Testing for successful instance collection");
 
     final Response r = RestAssured
@@ -274,12 +201,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesIdSearchFailTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Testing for successful instance collection");
 
     final Response r = RestAssured
@@ -304,12 +229,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesHandlesInvalidQueryTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Test when query is invalid, exception is thrown");
 
     RestAssured
@@ -325,12 +248,10 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 
   @Test
   public void getCodexInstancesHandlesUnsupportedQueryTest(TestContext context) {
-    final Async asyncLocal = context.async();
     logger.info("Test when query is invalid, exception is thrown");
 
     RestAssured
@@ -346,6 +267,5 @@ public class CodexInstanceResourceImplTest {
 
     // Test done
     logger.info("Test done");
-    asyncLocal.complete();
   }
 }
